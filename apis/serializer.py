7016fb35bfefd49d0d27a246e6b33cc84abf87f0
iamakhildabral <akhildabral02@gmail.com>
2023-12-11 20:57:17 +0530

    added swagger openapi

--- apis/serializer.py
+++ apis/serializer.py
@@ -6,4 +6,4 @@ from apis.models import Employee
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = ("id", "name", "address", "age", "salary")
--- databaseCon/urls.py
+++ databaseCon/urls.py
@@ -18,9 +18,32 @@ from django.contrib import admin
 from django.db import router
 from django.urls import include, path
 
-from apis import urls
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Employee Django REST API",
+        default_version='v1',
+        description="Testing planned for Swagger",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="test@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apis.urls'))
+    path('api/v1/', include('apis.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc')
 ]
